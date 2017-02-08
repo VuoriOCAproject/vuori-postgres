93d44e769bbc8b6e50914dd6ebfde8ac5c70e70f
siscokid760 <fhserrano@gmail.com>
2017-02-08 14:13:00 -0800

    etl process for ftp and pos

--- index.js
+++ index.js
@@ -1,41 +1,15 @@
-const extract = require('./lib/extract');
-const load = require('./lib/load');
+// To run lightspeed ETL process
+const etlLightspeed = require('./lib/etl-lightspeed');
+etlLightspeed
+  .run()
+  .then(() => {
+    console.log('Lightspeed ETL process complete.');
+  });
 
-Promise
-    .all([
-        extract.customers(),
-        extract.orderLineItems(),
-        extract.orders(),
-        extract.products()
-    ])
-    .then(data => {
-        return {
-            customers: data[0],
-            orderLineItems: data[1],
-            orders: data[2],
-            products: data[3]
-        };
-    })
-    .then(data => {
-        console.log('inserting customers into sql');
-        return load
-            .customers(data.customers)
-            .then(() => {
-                console.log('inserting products into sql');
-                return load.products(data.products)
-                    .then(() => {
-                        console.log('inserting orders into sql');
-                        return load.orders(data.orders)
-                            .then(() => {
-                                console.log('inserting order line items into sql');
-                                return load.orderLineItems(data.orderLineItems);
-                            });
-                    });
-            });
-    })
-    .then(() => {
-        console.log('finished.');
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+// To run test FTP process
+const etlFtp = require('./lib/etl-ftp');
+etlFtp
+  .run()
+  .then(() => {
+    console.log('Test FTP process complete.');
+  });
